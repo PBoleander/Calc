edc9ef35c6afd2e203fe5b81de4e72a19a7f5be8
Pascual Barrer <pbarrer@cifpfbmoll.eu>
2022-08-31 17:10:11 +0200

    Created MyStringBuilder.cs to improve the performance of Calculator.Calculate

--- Models/Calculator.cs
+++ Models/Calculator.cs
@@ -5,35 +5,36 @@ namespace Calc.Models;
 
 public static class Calculator
 {
-    public static string Calculate(string calc)
+    public static string Calculate(string str)
     {
+        var calculus = new MyStringBuilder(str);
+
         // ========== Manage parentheses ========== //
-        var numberOfOpeningParentheses = CountIn(calc, '(');
-        var numberOfClosingParentheses = CountIn(calc, ')');
+        var numberOfOpeningParentheses = calculus.Count('(');
+        var numberOfClosingParentheses = calculus.Count(')');
         
         if (numberOfOpeningParentheses != numberOfClosingParentheses)
             return "Waiting until all parentheses are closed";
         
         int indexOfOpeningParenthesis;
-        while ((indexOfOpeningParenthesis = calc.LastIndexOf("(", StringComparison.Ordinal)) != -1)
+        while ((indexOfOpeningParenthesis = calculus.LastIndexOf('(')) != -1)
         {
-            var indexOfClosingParenthesis = calc.IndexOf(")", indexOfOpeningParenthesis, StringComparison.Ordinal);
+            var indexOfClosingParenthesis = calculus.IndexOf(')', indexOfOpeningParenthesis);
             // Replace parentheses with its result
-            calc = calc[..indexOfOpeningParenthesis] +
-                   Calculate(calc[(indexOfOpeningParenthesis + 1)..indexOfClosingParenthesis]) +
-                   calc[(indexOfClosingParenthesis + 1)..];
+            calculus.Replace(indexOfOpeningParenthesis, indexOfClosingParenthesis + 1,
+                Calculate(calculus[(indexOfOpeningParenthesis + 1)..indexOfClosingParenthesis]));
         }
         
         // ========== Manage other operations ========== //
         int indexOfOperator;
         
         // Search calculations with precedence first. When there isn't more, continue with the others
-        while ((indexOfOperator = calc.IndexOfAny(OperatorChar.PrecedentOperators, 1)) > 0 || // precedent operations first
-               (indexOfOperator = calc.IndexOfAny(OperatorChar.NonPrecedentOperators, 1)) > 0)
+        while ((indexOfOperator = calculus.IndexOfAny(OperatorChar.PrecedentOperators, 1)) > 0 || // precedent operations first
+               (indexOfOperator = calculus.IndexOfAny(OperatorChar.NonPrecedentOperators, 1)) > 0)
         {
             // ==== Find the first operand ==== //
-            var indexOfPreviousOperator = SetIndexOfPreviousOperator(calc, indexOfOperator);
-            var stringOfFirstValue = calc[(indexOfPreviousOperator + 1)..indexOfOperator];
+            var indexOfPreviousOperator = SetIndexOfPreviousOperator(calculus, indexOfOperator);
+            var stringOfFirstValue = calculus[(indexOfPreviousOperator + 1)..indexOfOperator];
             var startIndexOfCalculation = indexOfPreviousOperator + 1;
 
             // First value could be just the sign -, e.g. in --3
@@ -46,28 +47,27 @@ public static class Calculator
             
             // ==== Find the second operand ==== //
             // startIndex = indexOfOperator + 2 avoids to detect sign of second value as operator
-            var indexOfNextOperator = calc.IndexOfAny(OperatorChar.Operators, indexOfOperator + 2);
+            var indexOfNextOperator = calculus.IndexOfAny(OperatorChar.Operators, indexOfOperator + 2);
 
             if (indexOfNextOperator == -1) // Last calculation
-                indexOfNextOperator = calc.Length;
+                indexOfNextOperator = calculus.Length;
             
-            var stringOfSecondValue = calc[(indexOfOperator + 1)..indexOfNextOperator];
+            var stringOfSecondValue = calculus[(indexOfOperator + 1)..indexOfNextOperator];
             var nextIndexAfterCalculation = indexOfNextOperator;
 
             // ==== Construct the calculation ==== //
             var firstValue = Convert.ToDouble(stringOfFirstValue);
-            var @operator = CharToOperator(calc[indexOfOperator]);
+            var @operator = CharToOperator(calculus[indexOfOperator]);
             var secondValue = Convert.ToDouble(stringOfSecondValue);
 
             var calculation = new Calculation(firstValue, secondValue, @operator);
             
             // Replace calculation with its result
-            calc = calc[..startIndexOfCalculation] +
-                   Convert.ToString(calculation.Calculate(), CultureInfo.CurrentCulture) +
-                   calc[nextIndexAfterCalculation..];
+            calculus.Replace(startIndexOfCalculation, nextIndexAfterCalculation,
+                Convert.ToString(calculation.Calculate(), CultureInfo.CurrentCulture));
         }
 
-        return calc;
+        return calculus.ToString();
     }
 
     private static Operator? CharToOperator(char? character)
@@ -81,21 +81,10 @@ public static class Calculator
             _ => null
         };
     }
-    
-    private static int CountIn(string s, char character)
-    {
-        var count = 0;
-        
-        foreach (var c in s)
-            if (c.Equals(character))
-                count++;
 
-        return count;
-    }
-
-    private static int SetIndexOfPreviousOperator(string calc, int indexOfOperator)
+    private static int SetIndexOfPreviousOperator(MyStringBuilder calculus, int indexOfOperator)
     {
-        var indexOfPreviousOperator = calc.LastIndexOfAny(OperatorChar.Operators, indexOfOperator - 1);
+        var indexOfPreviousOperator = calculus.LastIndexOfAny(OperatorChar.Operators, indexOfOperator - 1);
         
         // First calculation. There could not be an operator at the beginning, it must be a sign
         if (indexOfPreviousOperator == 0)
@@ -105,8 +94,8 @@ public static class Calculator
         // If the first value is negative and not the first calculation, an operator must be just before the index
         // previously calculated as the indexOfPreviousOperator, e.g. in a+-b/c the - isn't previousOperator, it's +
         else if (indexOfPreviousOperator > 0 &&
-                 calc[indexOfPreviousOperator].Equals(OperatorChar.Substract) && // minus sign
-                 OperatorChar.IsAnOperator(calc[indexOfPreviousOperator - 1])) // previous index contains an operator
+                 calculus[indexOfPreviousOperator].Equals(OperatorChar.Substract) && // minus sign
+                 OperatorChar.IsAnOperator(calculus[indexOfPreviousOperator - 1])) // previous index contains an operator
         {
             indexOfPreviousOperator--;
         }
